98142cbca02c587ff2085f7076538265bc7810a7
DanMO23 <115201102+DanMO23@users.noreply.github.com>
2023-06-03 15:21:32 -0300

    atualizada funcao read

--- entrada.asm
+++ entrada.asm
@@ -1,8 +1,12 @@
-2000:
-lw x1, 1000(x2)
-sw x1, 1000(x2)
-add x2, x0, x1
-sll X1, x2, x2
-xor x4, x2, x3
-addi x3, x2, -243
-bne x10, x11, 2000
+addi x2, x0, 7
+sw x2, 4(x0)
+lw x1, 4(x0)
+add x2, x1, x0
+add x1, x1, x2
+add x1, x1, x2
+add x1, x1, x1
+add x1, x1, x1
+add x1, x1, x1
+sw x1, 0(x0)
+SAIDA:
+sw x1, 0(x0)
